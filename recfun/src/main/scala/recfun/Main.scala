e447db111052e8aa0eaaf35d0ea3ff087b2ffec5
seekerlee <lishi1608@gmail.com>
2014-05-05 00:11:38 +0800

    done

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -9,20 +9,46 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println(balance(")".toList))
+    println(countChange(4, List(1, 2)))
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = (c, r) match {
+      case (0, _) => 1
+      case (_, 0) => 0
+      case (i, j) => pascal(i - 1, j - 1) + pascal(i, j - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance0(chars: List[Char], depth: Int = 0): Boolean = {
+      if(chars.isEmpty) {
+        depth == 0
+      }
+      else if(depth < 0) false
+      else if(chars.head == '(') balance0(chars.tail, depth + 1)
+      else if(chars.head == ')') balance0(chars.tail, depth - 1)
+      else balance0(chars.tail, depth)
+    }
+    if(chars.isEmpty) false
+    else balance0(chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(capacity: Int, changes: List[Int]): Int = {
+      if(capacity == 0) 1
+      else if(capacity < 0) 0
+      else if(changes.isEmpty && capacity>=1 ) 0
+      else count(capacity, changes.tail) + count(capacity - changes.head, changes)
+    }
+    count(money, coins.sortWith(_.compareTo(_) < 0))
+  }
 }
